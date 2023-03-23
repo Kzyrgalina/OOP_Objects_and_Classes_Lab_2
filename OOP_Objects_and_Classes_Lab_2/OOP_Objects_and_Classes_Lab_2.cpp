4d0ed6a4daeb1a598afa8d6a53a02916d0bfd37d
User <User@DESKTOP-CGQ0S7N>
2023-03-23 15:21:00 +0500

    first iter

--- OOP_Objects_and_Classes_Lab_2/OOP_Objects_and_Classes_Lab_2.cpp
+++ OOP_Objects_and_Classes_Lab_2/OOP_Objects_and_Classes_Lab_2.cpp
@@ -1,6 +1,68 @@
 ﻿#include <iostream>
+#include <conio.h>
+
+// создание класса
+class Point {
+protected:
+    int x, y; // два атрибута
+public: 
+    Point() { // конструктор без параметров
+            std::cout << "Constr Point()\n";
+            x = 0;
+            y = 0;
+    }
+
+    Point(int x, int y) { // конструктор с параметрами
+        std::cout << "Constr Point(int x, int y)\n";
+        this->x = x;
+        this->y = y;
+    }
+    Point(const Point &p) { // копирующий конструктор (переносит всне свойства)
+        std::cout << "Constr copy Point(const Point &p)\n";
+        x = p.x;
+        y = p.y;    
+    }
+    ~Point() {
+        //std::cout << " ~Point()\n";
+        printf("~Point(%d, %d)\n", x, y);
+    }
+    void move(int dx, int dy) {
+        x += dx;
+        y += dy;
+    }
+    void reset();
+};
+
+
+void Point::reset() {
+    x = 0;
+    y = 0;
+}
 
 int main()
 {
+    {
+    // статически
+    //Point p; 
+    //Point p2(10, 20);
+    //Point p3(p2);
+
+    // динамическое создание
+        Point *p1 = new Point(1,2);
+        //Point *p2 = new Point(10, 20);
+        //Point* p3 = new Point(*p2);
+
+
+    //методы
+        //p1->reset;
+        p1->move(10, 10);
+        
+    //удаляем сами, чтобы увидеть работу деструкторов
+        delete p1;
+        //delete p2;
+        //delete p3;
+    }
+    
+
     std::cout << "Hello World!\n";
 }
