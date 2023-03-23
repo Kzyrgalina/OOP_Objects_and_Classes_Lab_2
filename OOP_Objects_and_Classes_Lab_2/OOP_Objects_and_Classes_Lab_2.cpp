dc0bad0495601eb33fcff967d99e9129f67fc3cb
User <User@DESKTOP-CGQ0S7N>
2023-03-23 16:52:19 +0500

    second iter

--- OOP_Objects_and_Classes_Lab_2/OOP_Objects_and_Classes_Lab_2.cpp
+++ OOP_Objects_and_Classes_Lab_2/OOP_Objects_and_Classes_Lab_2.cpp
@@ -33,6 +33,44 @@ public:
     void reset();
 };
 
+class ExtraPoint: public Point{
+protected:
+    int z; 
+public:
+    ExtraPoint(): Point() { // конструктор без параметров
+        std::cout << "Constr ExtraPoint()\n";
+        //x = 0;
+        //y = 0;
+        z = 0;
+    }
+
+    ExtraPoint(int x, int y, int z): Point() { // конструктор с параметрами
+        std::cout << "Constr ExtraPoint(int x, int y, int z)\n";
+        //this->x = x;
+        //this->y = y;
+        this->y = z;
+    }
+    ExtraPoint(const ExtraPoint& p) : Point(x, y) { // копирующий конструктор (переносит всне свойства)
+        std::cout << "Constr copy ExtraPoint(const Point &p)\n";
+        x = p.x;
+        y = p.y;
+        z = p.z;
+    }
+    ~ExtraPoint() {
+        //std::cout << " ~Point()\n";
+        printf("~Point(%d, %d, %d)\n", x, y, z);
+    }
+    void move(int dx, int dy, int dz){
+        x += dx;
+        y += dy;
+        z += dz;
+    }
+    void reset();
+    void changeZ(int newz) {
+        z = newz;
+    }
+};
+
 
 void Point::reset() {
     x = 0;
@@ -48,19 +86,22 @@ int main()
     //Point p3(p2);
 
     // динамическое создание
-        Point *p1 = new Point(1,2);
+        //Point *p1 = new Point(1,2);
         //Point *p2 = new Point(10, 20);
         //Point* p3 = new Point(*p2);
 
+        ExtraPoint* ep = new ExtraPoint(1, 2, 3);
+        
 
     //методы
-        //p1->reset;
-        p1->move(10, 10);
+        //p1->reset();
+        //p1->move(10, 10);
         
     //удаляем сами, чтобы увидеть работу деструкторов
-        delete p1;
+        //delete p1;
         //delete p2;
         //delete p3;
+        delete ep;
     }
     
 
